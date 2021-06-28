ce12940ced1c234edc7d0246b63b6dab7fbaa902
Conner Charlebois <conner.charlebois@c02dr7ummd6t.lan>
2021-06-28 13:32:36 -0400

    set to initial state

--- src/CalendarStrip.jsx
+++ src/CalendarStrip.jsx
@@ -1,5 +1,5 @@
 import { Component, createElement } from "react";
-import Cal from 'react-native-calendar-strip';
+
 import { shouldDateShowMark, calStyle, daySelectionAnimation } from "./helpers/helpers";
 
 
@@ -9,18 +9,5 @@ export const CalendarStrip = ({ attribute, action, datasource, dateAttr }) => {
         action.execute();
     }
     const markedDates = date => shouldDateShowMark(date, datasource, dateAttr);
-    return (
-        <Cal
-            scrollable
-            style={calStyle}
-            selectedDate={attribute.value}
-            onDateSelected={updateAttribute}
-            daySelectionAnimation={daySelectionAnimation}
-            calendarColor={'#36b9cc'}
-            markedDates={markedDates}
-            iconLeft={{uri: require("./assets/left-arrow.png")}}
-            iconRight={{uri: require("./assets/right-arrow.png")}}
-            iconContainer={{flex: 0.1}}
-        />
-    );
+    return null;
 }
